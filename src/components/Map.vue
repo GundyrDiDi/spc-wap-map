ee24d2444998adfce4daad0c968dcc7a02224515
GundyrDiDi <55871227+GundyrDiDi@users.noreply.github.com>
2020-03-11 15:09:47 +0800

    add ol

---
+++ src/components/Map.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="map">
+    map
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Map'
+}
+</script>
+
+<style scoped>
+  #map {
+    height: 100px;
+    background: red;
+  }
+</style>
