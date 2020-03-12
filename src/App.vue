623d88fad06e0fdeda034579c4a0677dfd0180e6
GundyrDiDi <55871227+GundyrDiDi@users.noreply.github.com>
2020-03-12 16:13:59 +0800

    add axios

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <Map/>
+    <Map />
   </div>
 </template>
 
@@ -11,9 +11,40 @@ export default {
   name: 'app',
   components: {
     Map
+  },
+  computed: {
+
+  },
+  mounted () {
+
   }
 }
+
 </script>
 
 <style>
+  html {
+    position: relative;
+    overflow: hidden;
+    top: 0;
+    left: 0;
+    width: 100%;
+    user-select: none;
+    -webkit-user-select: none;
+  }
+
+  body {
+    margin: 0;
+    padding: 0;
+    height: 100vh;
+    font-family: 'Microsoft Yahei';
+    position: relative;
+    overflow: hidden;
+    width: 100%;
+  }
+
+  :root {
+    font-size: 18px;
+  }
+
 </style>
--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,12 +1,30 @@
 <template>
   <div id="map">
-    map
   </div>
 </template>
 
 <script>
+import 'ol/ol.css'
+import Map from 'ol/Map'
+import View from 'ol/View'
+
 export default {
-  name: 'Map'
+  name: 'Map',
+  data () {
+    return {
+      map: undefined
+    }
+  },
+  mounted () {
+    this.map = new Map({
+      layers: [],
+      target: 'map',
+      view: new View({
+        center: [-6655.5402445057125, 6709968.258934638],
+        zoom: 13
+      })
+    })
+  }
 }
 </script>
 
