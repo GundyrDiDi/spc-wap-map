edbf6ce8e44589d8878398711aaddd1cc1f550ff
GundyrDiDi <55871227+GundyrDiDi@users.noreply.github.com>
2020-03-29 15:33:14 +0800

    add lottie

---
+++ src/components/lottie-loading.vue
@@ -0,0 +1,28 @@
+<template>
+<div id="overlay">
+  <transition name="el-fade-in">
+    <lottie :options="defaultOptions" :height="200" :width="200"></lottie>
+  </transition>
+</div>
+</template>
+
+<script>
+import lottie from 'vue-lottie'
+import maploading from '../assets/1.json'
+export default {
+  name: 'lottie-loading',
+  components: {
+    lottie
+  },
+  data () {
+    return {
+      defaultOptions: { animationData: maploading },
+      animationSpeed: 1
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/ol-map.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="map" ref="el">
+  </div>
+</template>
+
+<script>
+import 'ol/ol.css'
+
+export default {
+  name: 'Map',
+  mounted () {
+    this.map_init({ el: this.$refs.el })
+  }
+}
+</script>
+
+<style scoped>
+  #map {
+    position:absolute;
+    top:0;
+    left:0;
+    width:100%;
+    height:100%;
+    background:#000;
+  }
+</style>
